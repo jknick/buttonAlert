cf77f2b52fb35501014792326e7e0ffa67a0f1e1
Jake Knickerbocker <jake.knickerbocker@gmail.com>
2015-04-08 10:33:08 -0500

    Formatting change.

--- main.cpp
+++ main.cpp
@@ -10,19 +10,19 @@
 
 int main(int argc, char* argv[])
 {
-	QGuiApplication app(argc, argv);
-	QQuickView view;
+    QGuiApplication app(argc, argv);
+    QQuickView view;
 
 
     FileIO fileIO;
     view.rootContext()->setContextProperty("fileio", &fileIO);
 
-	view.connect(view.engine(), SIGNAL(quit()), &app, SLOT(quit()));
-	//view.setSource(QUrl::fromLocalFile("./main.qml"));
+    view.connect(view.engine(), SIGNAL(quit()), &app, SLOT(quit()));
+    //view.setSource(QUrl::fromLocalFile("./main.qml"));
 
-	view.setSource(QUrl("qrc:////qml/main.qml"));
-	view.setResizeMode(QQuickView::SizeRootObjectToView);
-	view.show();
+    view.setSource(QUrl("qrc:////qml/main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.show();
 
-	return app.exec();
+    return app.exec();
 }
